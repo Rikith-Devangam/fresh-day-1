fe01f96ef6d05da5aa568a80ed5e29b3ac763109
Rikith Devangam <rikithdevangam@Rikiths-MacBook-Air.local>
2024-07-25 00:01:18 +0530

    Basics of functions in python

---
+++ func.py
@@ -0,0 +1,36 @@
+# Review: 
+# Create a function called greet(). 
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+
+
+
+# def greet():
+#   print("Hello")
+#   print("How do you do ?")
+#   print("Isn't the weather great today")
+
+# greet()
+
+#--------------------------------------------------------------------------------
+# def greet_with_name(name):
+#   print(f"Hello {name}")
+#   print(f"How do you do { name}?")
+
+# greet_with_name("Jack Bauer")
+#---------------------------------------------------------------------------------
+
+# name = input ("what is your name")
+# def greet_with_name(name):
+#   print(f"Hello {name}")
+#   print(f"How do you do { name}?")
+
+# greet_with_name(name)
+#----------------------------------------------------------------------------------
+def greet_with(name , location):
+  print(f"hello {name}")
+  print(f"how is it like in {location}")
+#greet_with("Jack Bauer" , "Nowhere")
+#greet_with("Nowhere" , "jack Bauer")
+greet_with(location ="London" , name = "Angelia")
+
